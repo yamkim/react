c0b84fa45ca547e78e49d55a117bdd06568f3c03
yamkim <dev.yamkim@gmail.com>
2022-12-04 08:48:09 +0900

    feat: [tsx setting] emotion, twin.macro 추가하기

--- tsx_version/src/pages/HomePage.tsx
+++ tsx_version/src/pages/HomePage.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import tw, { css } from "twin.macro";
 
 const HomePage = () => {
-    return <div className="text-3xl font-bold underline">
-        hello
-    </div>;
+    return (
+        <div
+        className=""
+        css={[
+            tw`text-3xl font-bold underline`,
+            css`
+                background: #ff802c;
+            `,
+        ]}
+        >
+        Hello world
+        </div>
+    );
 };
 
 export default HomePage;
