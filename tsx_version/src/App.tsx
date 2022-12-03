d1b981c8b80bf1706a8422f6ab1b3757adcc168e
yamkim <dev.yamkim@gmail.com>
2022-12-03 22:18:09 +0900

    feat: [tsx setting] npm start를 위한 기본 세팅 적용

---
+++ tsx_version/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
+import HomePage from "./pages/HomePage"
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ tsx_version/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <>
+            hello
+        </>
+    );
+};
+
+export default HomePage;
